371665548d296a281037886f732b6d1d4cd53a28
Kevin Cazelles <kcazelle@uoguelph.ca>
2017-07-09 17:43:30 -0400

    add js scripts

---
+++ static/js/customfooter.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $('footer:last').append("<p> All contents licensed under "+
+   "<a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>" +
+   "<i class='cc cc-cc-circle cc-1x'></i> <i class='cc cc-by-circle cc-1x'></i> "+
+   "<i class='cc cc-nc-circle cc-1x'></i> <i class='cc cc-sa-circle cc-1x'></i>"+
+   "</a> unless otherwise specified. <br/>"+
+   "Code source of this website available on <a href='https://github.com/KevCaz/kevcazHugoWebsite'>Github</a> </p>");
+});
--- static/js/mapoffice.js
+++ static/js/mapoffice.js
@@ -1,24 +1,22 @@
-// Longitude/Latitude UQAR
-var latlngu = L.latLng(48.452900, -68.512790);
-var zoom = 5;
+var latlngu = [43.5299, -80.2286];
+var zoom = 14;
 
-
-// set up the map and remove the default zoomControl
 var map = L.map('map', {
-    zoomControl: false
-});
-map.setView(latlngu, zoom);
+  // zoomControl: false
+}).setView(latlngu, zoom);
 
-// add an OpenStreetMap tile layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://www.openstreetmap.org/#map=17/48.45315/-68.51316">View larger map</a> '
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://www.openstreetmap.org/#map=17/43.53012/-80.22782">View larger map</a> '
 }).addTo(map);
 
-// add a marker in the given location, attach some popup content to it and open the popup
 L.marker(latlngu).addTo(map)
-    .bindPopup('<a href="http://www.uqar.ca"><b>UQAR</b></a> <br/> Campus de Rimouski <br/> 300, allée des Ursulines <br/> G5L 3A1 Rimouski, Qc (Canada) <br/> <i class="fa fa-phone fa"></i>  &nbsp; +1 418 723-1986 (1569) ')
+    .bindPopup('<a href="https://www.uoguelph.ca/"><b>University of Guelph</b></a> <br/>'+
+    '<a href="https://www.uoguelph.ca/ib/"> Department of Integrative Biology</a><br/>'+
+    'Summerlee Science Complex, <br/> Office SSC-2471, <br/> Guelph, ON N1G 2W1')
     .openPopup();
-//
+
 // add the new control to the map
 var zoomHome = new L.Control.zoomHome(latlngu, zoom);
 zoomHome.addTo(map);
+
+address = "Department of Integrative Biology, Summerlee Science Complex, "
