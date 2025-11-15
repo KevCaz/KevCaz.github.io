f15fbcfe4dadc731df43613d7d916a9641c47c66
KevCaz <KevCaz@users.noreply.github.com>
2025-11-15 20:47:52 +0000

    deploy: 3ced138c326adf2b890394da41e2a9dfdca5b738

--- js/sidebar-primary-dropdown.js
+++
@@ -1,6 +0,0 @@
-// https://www.w3schools.com/howto/howto_js_dropdown.asp
-// https://stackoverflow.com/questions/11212470/double-click-to-show-an-div
-function dropDown(id) {
-    var el = document.getElementById(id);
-    el.style.display = (el.style.display == 'none' || el.style.display == '') ? 'block' : 'none';
-  }
